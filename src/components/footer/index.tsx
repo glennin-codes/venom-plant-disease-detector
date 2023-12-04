6c82592251e9d6ad2f68692e9603ebd75c487c14
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-12-04 04:00:28 +0300

    background image center

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -77,7 +77,7 @@ export const Footer = () => {
                 <input
                   type="text"
                   className="flex-1 px-4 rounded-full focus:outline-none"
-                  placeholder="Updated in your inbox"
+                  placeholder="subscribe to our email"
                 />
                 <button className="px-6 py-2 text-white rounded-full bg-greenMain hover:bg-greenMainLight focus:outline-none">
                   Go
