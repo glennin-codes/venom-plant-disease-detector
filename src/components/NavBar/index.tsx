d7b5185dfe3df9f9fbe0e53a5f96d019083efe2c
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-12-04 03:36:28 +0300

    background opacity

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -50,7 +50,7 @@ export const NavBar=()=>{
       <div className={`md:hidden `}>
         <div
           id="menu"
-          className={`absolute  font-Poppins flex-col items-center ${isMenuOpen ? 'flex' : 'hidden'} self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}
+          className={`absolute  font-Poppins flex-col items-center ${isMenuOpen ? 'flex' : 'hidden'} self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 bg-opacity-50`}
         >
           <a href="#">Pricing</a>
           <a href="#">Product</a>
