24964c5a3a409b3a4d04c17726195728854ed6c4
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-12-04 03:33:24 +0300

    fixed  and issue

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -50,13 +50,14 @@ export const NavBar=()=>{
       <div className={`md:hidden `}>
         <div
           id="menu"
-          className={`absolute  font-Poppinsflex-col items-center ${isMenuOpen ? 'flex' : 'hidden'} self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}
+          className={`absolute  font-Poppins flex-col items-center ${isMenuOpen ? 'flex' : 'hidden'} self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md`}
         >
           <a href="#">Pricing</a>
           <a href="#">Product</a>
           <a href="#">About Us</a>
           <a href="#">Careers</a>
           <a href="#">Community</a>
+          <Link  to="/login">Login</Link>
         </div>
       </div>
             
